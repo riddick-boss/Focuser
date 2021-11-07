ba5a98a79962a1335cbdf93a0d4eb5ed159de30f
Pawel Kremienowski <kremienowski33@gmail.com>
2021-11-07 21:42:59 +0100

    MethodFragment.kt setup

--- app/src/main/java/abandonedstudio/app/focuser/ui/focusmethods/method/MethodFragment.kt
+++ app/src/main/java/abandonedstudio/app/focuser/ui/focusmethods/method/MethodFragment.kt
@@ -1,15 +1,17 @@
 package abandonedstudio.app.focuser.ui.focusmethods.method
 
 import abandonedstudio.app.focuser.databinding.MethodBinding
+import abandonedstudio.app.focuser.model.room.focusmethod.FocusMethod
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MethodFragment : Fragment() {
@@ -31,11 +33,20 @@ class MethodFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Toast.makeText(requireContext(), args.methodId.toString(), Toast.LENGTH_SHORT).show()
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            loadMethodUI(viewModel.getMethod(args.methodId))
+        }
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    //    TODO: try to reorganize code, so importing FocusMethod will NOT be needed
+    private fun loadMethodUI(method: FocusMethod) {
+        binding.nameTV.text = method.name
     }
 }
--- app/src/main/java/abandonedstudio/app/focuser/ui/focusmethods/method/MethodViewModel.kt
+++ app/src/main/java/abandonedstudio/app/focuser/ui/focusmethods/method/MethodViewModel.kt
@@ -1,9 +1,16 @@
 package abandonedstudio.app.focuser.ui.focusmethods.method
 
+import abandonedstudio.app.focuser.model.room.focusmethod.FocusMethod
+import abandonedstudio.app.focuser.model.room.focusmethod.FocusMethodRepository
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class MethodViewModel @Inject constructor() : ViewModel() {
+class MethodViewModel @Inject constructor(private val focusMethodRepository: FocusMethodRepository) :
+    ViewModel() {
+
+    suspend fun getMethod(methodId: Int): FocusMethod {
+        return focusMethodRepository.getMethod(methodId)
+    }
 }
